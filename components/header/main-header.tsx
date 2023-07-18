75242d9be0b1ea40b58abe3f0e8789e647485f5f
Max <allenmaxnichols@gmail.com>
2023-07-18 15:24:10 -0500

    add some styles and components

--- components/header/main-header.tsx
+++ components/header/main-header.tsx
@@ -1,42 +1,38 @@
 import Link from "next/link";
-
+import styles from './main-header.module.scss';
+import Container from "../container/container";
 export default function MainHeader() {
     return (
-        <header>
-            <h1>
-                <Link href="/">
-                    Yelp Camp
-                </Link>
-            </h1>
-            <nav>
-                <ul>
-                    <li>
-                        <Link href="/campgrounds">
-                            Campgrounds
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/campgrounds/new">
-                            New Campground
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/register">
-                            Sign Up
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/login">
-                            Sign In
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/logout">
-                            Sign Out
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+        <Container>
+            <header className={styles.mainHeader}>
+                <h1>
+                    <Link href="/">
+                        Yelp Camp
+                    </Link>
+                </h1>
+                <nav>
+                    {/* <ul>
+                        <li>
+                            <Link href="/campgrounds">
+                                Campgrounds
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/campgrounds/new">
+                                New Campground
+                            </Link>
+                        </li>
+                    </ul> */}
+                </nav>
+                <div className={styles.buttonContainer}>
+                    <Link href="/auth/signin">
+                        Sign In
+                    </Link>
+                    <Link href="/auth/register" className={styles.registerBtn}>
+                        Sign Up
+                    </Link>
+                </div>
+            </header>
+        </Container>
     );
 }
